760bea45898c489f60f383b324b57986ff65c98c
Muhammed Shammas <95122543+Shammaz77@users.noreply.github.com>
2023-01-10 14:35:28 +0530

    cart page + Responsive ~ ui completed!

--- js/main.js
+++ js/main.js
@@ -1,3 +1,41 @@
 function toggleCartt(){
     document.querySelector('.sidebar').classList.toggle('open-cart');
 }
+// cart increase 
+$(document).ready(function(){
+    var n = 0;
+    var adultCounter = ".counter-adult";
+    var childCounter = ".counter-child";
+
+    $(adultCounter).val(n);
+    $(childCounter).val(n);
+
+    $(".plus-adult").on("click", function(){
+            $(adultCounter).val(++n);
+    })
+    $(".minus-adult").on("click", function(){
+            if (n > 0) {
+                    $(adultCounter).val(--n);
+            }
+    })
+
+
+
+    $(".plus-child").on("click", function(){
+            $(childCounter).val(++n);
+    })
+    $(".minus-child").on("click", function(){
+            if (n > 0) {
+                    $(childCounter).val(--n);
+            }
+    })
+
+    if ($(childCounter).val(n) > 0) {$(".minus-child").removeClass("de-active-counter")}
+    
+});
+function increment() {
+    document.getElementById('demoInput').stepUp();
+  }
+  function decrement() {
+    document.getElementById('demoInput').stepDown();
+  }
